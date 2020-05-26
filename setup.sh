f642ecb01a403feac3d4ec82a46443dceb9c2764
officialnico <24nico@gmail.com>
2020-05-26 07:11:11 +0000

    setup fixed

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
-sudo su -
+#!/bin/sh
 apt-get update
-apt-get install python3
+apt-get install python
 git clone https://github.com/trustedsec/ptf /opt/ptf
 cd /opt/ptf && ./ptf
 use modules/exploitation/install_update_all
-use modules/intelligence-gathering/install_update_all
-use modules/post-exploitation/install_update_all
-use modules/powershell/install_update_all
-use modules/vulnerability-analysis/install_update_all
-cd /pentest
+:
